3098be060f5b71dad146478d83ee710b46c1e021
Noah Colvin <7552226+noahcolvin@users.noreply.github.com>
2025-02-16 14:35:01 -0700

    Additional cleanup

--- supabase/functions/_shared/data/database.ts
+++ supabase/functions/_shared/data/database.ts
@@ -1,28 +1,3 @@
-/* import { Database } from './types.ts';
-// @ts-types="npm:@types/pg"
-import { Pool, types } from 'npm:pg';
-import { Kysely, PostgresDialect } from 'npm:kysely@^0.27.4';
-
-types.setTypeParser(types.builtins.INT4, (val: string) => {
-  return parseFloat(val);
-});
-
-const dialect = new PostgresDialect({
-  pool: new Pool({connectionString:'',
-    database: 'postgres',
-    host: 'localhost',
-    user: 'postgres',
-    password: 'postgres',
-    port: 54322,
-    max: 10,
-  }),
-});
-
-export const db = new Kysely<Database>({
-  dialect,
-});
- */
-
 import { drizzle } from "npm:drizzle-orm/postgres-js";
 import postgres from "npm:postgres";
 
